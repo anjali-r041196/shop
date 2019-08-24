d42679493afeebe1c66a4d0b405e6604ca176417
anjana-04 <anjanarajasekhar041196@gmail.com>
2019-08-24 23:44:50 +0530

    klll

--- routes/womenrouter.js
+++ routes/womenrouter.js
@@ -1,67 +1,27 @@
 var express=require('express');
 const router=express.Router();
-var pal=[{image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-},
-{
-image:"https://5.imimg.com/data5/HB/HX/MY-57848307/boy-kids-collar-suit-500x500.jpg",
-title:"T-shirt",
-price:"100",
-body:"A cool stripped T-shirt for boys"
-
-}
-]
-
+var mongoose=require("mongoose")
+var bodyparser=require("body-parser")
+var url="mongodb://127.0.0.1:27017/shopping"
+var user3=require("../model/user3")
+router.use(bodyparser.urlencoded({extended:true}))
 router.get("/",function(req,res){
-res.render("men",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}],p:pal})
+    user3.find({},function(err,result){
+       if(err) throw err
+       else{
+    res.render("women",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}],res:result})  
+       }
+    })
 })
-router.get("/:id",function(req,res){
-var id=req.params.id
-res.render("singlem",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}],p:pal[id]})
+router.get("/singlew/:id",function(req,res){
+    var s=req.params.id
+    user3.find({},function(err,result){
+       if(err) throw err
+       else{
+    res.render("singlew",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}],res:result[s]})  
+       }
+    })
 })
-router.get("/",function(req,res){
-    res.render("women",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}]})
-})
-router.get("/:id",function(req,res){
-    var id=req.params.id
-    res.render("singlew",{nav:[{link:"/men",title:"men wear"},{link:"/women",title:"women wear"},{link:"/child",title:"kids wear"}],p:pal[id]})
-})
+
+
 module.exports=router;
